5f1e66ea9ecb267dd0cc615bf7ad4ffdec879009
GeMixl <46502017+GeMixl@users.noreply.github.com>
2024-08-14 11:52:37 +0200

    improved search

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,9 @@ if ingredient_list:
         ingredients_string += fruits_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ' + fruits_chosen + ' is ' + search_on + '.')
-
+        
         st.subheader(fruits_chosen + ' Nutrition Information:')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values('""" + ingredients_string + """', '""" + name_on_smoothie + """');"""
